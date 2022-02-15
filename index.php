24dbfe56d86b8c368757a8f8ce1a88d925d2cb78
André Sünnemann <a.suennemann@edv-peuker.de>
2022-02-15 16:11:09 +0100

    update search and add a logger

--- index.php
+++ index.php
@@ -3,14 +3,23 @@
 require_once(__DIR__ . '/configuration.php');
 
 use DBS2\Database\Database;
+use DBS2\Debug\Logger;
 
 session_start();
+
+if(!isset($_SESSION['dbType']))
+{
+    header('Location: /frontend_php/settings.php');
+}
 
 ?>
 
 <?php require_once(__DIR__ . '/frontend_php/header.inc.php'); ?>
 <div class="wrapper">
-
+    <div class="description">
+        <p>This is a little search engine for the MITRE ATT&CK database (https://attack.mitre.org)</p>
+        <p>Use sql like syntax for your search (*, %, AND, OR, ...)</p>
+    </div>
     <div class="search-form">
         <form action="index.php" method="post">
             <div class="form-group">
@@ -34,12 +43,20 @@ session_start();
         // check if form was submitted
         if($_SERVER['REQUEST_METHOD'] == 'POST')
         {
+
             $search = $_POST['search'];
             $limitResult = $_POST['limitResult'];
 
             $dbCon = new Database($CONFIG['db'][$_SESSION['dbType']]['type']);
-            $dbCon->select(array('*'), 'mitigates');
+            $dbCon->select(array('*'), 'targets_industrytype')
+                ->where('industrytype LIKE \'' . $search . '\'');
+            if(intval($limitResult) > 0)
+            {
+                $dbCon->limit($limitResult);
+            }
 
+            $logger = new Logger();
+            $logger->log('New search: "' . $dbCon->getQueryStr() . '" from client ip ' . $_SERVER['REMOTE_ADDR'] . "\n");
             $dbCon->query();
             $resultArray = $dbCon->fetchArray();
             print_r($resultArray);
